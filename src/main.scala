8cd660f5a4b4c2197666036cf09005b4a753a749
Nicolas Harraudeau <nicolas.harraudeau@sonarsource.com>
2019-03-19 13:51:55 +0100

    add some code

---
+++ src/main.scala
@@ -0,0 +1,13 @@
+object HelloWorld {
+    def main(args: Array[String]): Unit = {
+        var myVar : String = "Foo";
+    }
+
+    def validate(input: String): Unit = {
+        if (input != null) {
+            if (input == "test") {
+                /* ... */
+            }
+        }
+    }
+}
